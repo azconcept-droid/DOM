b4744b7ce4784ab10ab714e2cd248a02d63f48d6
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-07 10:33:22 +0100

    accessing DOM children

---
+++ DOMChildren.js
@@ -0,0 +1,17 @@
+let children = document.body.children
+console.log(children[0])
+console.log(children[1])
+console.log(children[2])
+
+for (const elem of children) {
+    console.log(elem)
+}
+
+let child = document.body.firstElementChild
+console.log(child)
+
+let childNodes = document.body.childNodes
+console.log(childNodes[1])
+console.log(childNodes[3])
+
+
